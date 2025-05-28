ad3e981182cf2f5e37cf8788379318c5b73351fe
dristi srivastava <dristidpsbsc2003@gmail.com>
2025-05-29 00:35:50 +0530

    Update mongodb

--- server/server.js
+++ server/server.js
@@ -26,13 +26,14 @@ if (!fs.existsSync(uploadsDir)) {
   fs.mkdirSync(uploadsDir);
 }
 
-// MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('Failed to connect to MongoDB', err));
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log("MongoDB connected successfully");
+}).catch((err) => {
+  console.error("MongoDB connection error:", err);
+});
 
 // Start server
 app.listen(PORT, () => {
